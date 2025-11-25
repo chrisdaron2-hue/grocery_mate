3c00fa38321dd1bf562387a36440770b7f47721a
Elizabeth <chrisdaron2@gmail.com>
2025-11-25 19:00:25 +0100

    Add app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to GroceryMate!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
